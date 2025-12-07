1590561905efdbf863746a6ec2967a559ddcdfce
fasal <muhammedfasalkp710@gmail.com>
2025-12-07 23:19:17 +0530

    update socket.io path to work in production

--- src/config/socket.js
+++ src/config/socket.js
@@ -1,6 +1,9 @@
 import { io } from "socket.io-client";
 import { BASEURL } from "../constants/constants";
 
-const initializeSocket = () => io(BASEURL);
+const initializeSocket = () =>
+  window.location.hostname === "localhost"
+    ? io(BASEURL)
+    : io("/", { path: "/api/socket.io" });
 
 export default initializeSocket;
